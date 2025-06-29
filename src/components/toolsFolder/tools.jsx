c364c971ced8826c3302d3d511ee639a67c1d922
Antony Leon <aleonelgranados@gmail.com>
2025-06-30 00:26:39 +0200

    Ajustar estilos en la sección de herramientas: aumentar altura y mejorar efectos de hover

--- src/components/toolsFolder/tools.jsx
+++ src/components/toolsFolder/tools.jsx
@@ -6,7 +6,7 @@ const Tools = () => {
     <section className='tools-section'>
       <div className='tool-card'>
         <h3 className='s-title'>
-          Text To Speach</h3>
+          Dictador de texto</h3>
         <p>Una herramienta exelente para estudiantes de educación media, convierte texto en voz para que puedas copiarlo fácilmente en tu cuaderno mientras escuchas, puedes ajustarlo a tu ritmo sin perderte, pruebalo ahora.</p>
         <a target='_blank' href="https://dictador2.vercel.app/"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"></path></svg></a>
       </div>
